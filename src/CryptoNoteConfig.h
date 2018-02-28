87aa72043551980827f6a83ac8a8f68b114704f4
danthegeek <danth3g33k@gmail.com>
2018-02-28 13:27:14 -0500

    Add files via upload

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -17,7 +17,7 @@ namespace parameters {
   const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 600000000;
   const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 600000000;
   const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 2000000000;
-  const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x8; // addresses start with "M"
+  const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0xd7; // addresses start with "M"
   const size_t   CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 10;
   const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
   const uint64_t CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE           = 100;
@@ -31,8 +31,8 @@ namespace parameters {
   const uint64_t TAIL_EMISSION_REWARD                          = 0;
   const size_t CRYPTONOTE_COIN_VERSION                         = 0;
   const bool ZAWY_DIFFICULTY_V2                          	     = 0;
-  const unsigned EMISSION_SPEED_FACTOR                         = 18;
-  const uint64_t GENESIS_BLOCK_REWARD                          = 10000;
+  const unsigned EMISSION_SPEED_FACTOR                         = 18446744;
+  const uint64_t GENESIS_BLOCK_REWARD                          = 0;
   static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
   const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 0.1;
